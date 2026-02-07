03de7738f810b591a3631a68be04a7b2e03b0583
Richi <rloretor.dev@gmail.com>
2026-02-07 14:08:34 +0100

    Move BeatManager from Metronome.cs to Scripts/BeatManager.cs
    Consolidate the active BeatManager class into its proper location,
    replacing the old commented-out version. Preserved the script GUID
    so scene and prefab references remain intact.

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -1,136 +1,106 @@
-// using System;
-// using System.Collections;
-// using UnityEngine;
-//
-// public class BeatManager : MonoBehaviour
-// {
-//     public event Action OnPreBeat;
-//     public event Action OnBeat;
-//     public event Action OnPostBeat;
-//
-//     [SerializeField, Range(0, 120)] private float gameBPM = 120;
-//     [SerializeField, Range(0.0f, 1.0f)] public float MoveWindowSeconds;
-//     [Range(1, 100)] public float MoveWindowTimePercent = 10;
-//
-//     [SerializeField] private GameController gameController;
-//     [SerializeField] private SoundManager soundManager;
-//
-//     private Coroutine BeatCoroutine;
-//     [NonSerialized] public float LastBeatTime;
-//     [NonSerialized] public float NextBeatTime;
-//
-//     public float BeatInterval;
-//     public int BeatCounter { get; private set; }
-//
-//     [Header("Debug")] public bool ResetBeatCoroutine = false;
-//     public bool PlayBeatDeBugSound = false;
-//     [SerializeField] private GameObject BeatDebug;
-//
-//     private void OnGUI()
-//     {
-//         MoveWindowSeconds = MoveWindowTimePercent * BeatInterval / 100f;
-//         if (ResetBeatCoroutine)
-//         {
-//             ResetBeatCoroutine = false;
-//             InitBeatValues();
-//             StopCoroutine(BeatCoroutine);
-//             BeatCoroutine = StartCoroutine(StartBeatLoop());
-//         }
-//     }
-//
-//     public void Init()
-//     {
-//         InitBeatValues();
-//         OnBeat -= DebugBeat;
-//         OnBeat += DebugBeat;
-//
-//
-//         BeatCoroutine = StartCoroutine(StartBeatLoop());
-//     }
-//
-//     private void InitBeatValues()
-//     {
-//         BeatInterval = 60.0f / gameBPM;
-//         NextBeatTime = Time.time + BeatInterval;
-//         MoveWindowSeconds = MoveWindowTimePercent * BeatInterval / 100f;
-//     }
-//
-//     public void DebugBeat()
-//     {
-//         if (PlayBeatDeBugSound)
-//         {
-//             //soundManager.PlaySfx(SoundManager.Sfx.DebugBeat3);
-//         }
-//     }
-//
-//     private IEnumerator StartBeatLoop()
-//     {
-//         while (gameController.IsGameOver == false)
-//         {
-//             OnBeat?.Invoke();
-//
-//             LastBeatTime = Time.time;
-//             NextBeatTime = LastBeatTime + BeatInterval;
-//             BeatCounter++;
-//
-//             yield return new WaitForSeconds(MoveWindowSeconds);
-//
-//             OnPostBeat?.Invoke();
-//
-//             yield return new WaitForSeconds(BeatInterval - MoveWindowSeconds * 2);
-//
-//             OnPreBeat?.Invoke();
-//
-//             yield return new WaitForSeconds(MoveWindowSeconds);
-//         }
-//
-//         BeatCounter = 0;
-//     }
-//
-//     public void Update()
-//     {
-//         var currentTime = Time.time;
-//         var timeOfLastUpdate = Time.time - Time.deltaTime;
-// /*
-//         var preBeatTime = NextBeatTime - MoveWindowSeconds;
-//         if (currentTime >= preBeatTime && timeOfLastUpdate <= preBeatTime)
-//         {
-//             OnPreBeat?.Invoke();
-//             //soundManager.PlaySfx(SoundManager.Sfx.DebugBeat1);
-//
-//         }
-//
-//         if (currentTime >= NextBeatTime && timeOfLastUpdate <= NextBeatTime)
-//         {
-//             OnBeat?.Invoke();
-//             soundManager.PlaySfx(SoundManager.Sfx.DebugBeat3);
-//
-//             LastBeatTime = currentTime;
-//             NextBeatTime = LastBeatTime + BeatInterval;
-//             BeatCounter++;
-//         }
-//
-//         var postBeatTime = LastBeatTime + MoveWindowSeconds;
-//         if (currentTime >= postBeatTime && timeOfLastUpdate <= postBeatTime)
-//         {
-//             OnPostBeat?.Invoke();
-//             //soundManager.PlaySfx(SoundManager.Sfx.DebugBeat2);
-//
-//         }*/
-//
-//         // var currentTime = Time.time;
-//         var lapsedTimeSinceBeat = currentTime - LastBeatTime;
-//         var timeUntilNextBeat = NextBeatTime - currentTime;
-//         var timeToClosestBeat = Mathf.Min(lapsedTimeSinceBeat, timeUntilNextBeat);
-//         var moveWindowSeconds = MoveWindowTimePercent * BeatInterval / 100;
-//
-//         if (timeToClosestBeat <= moveWindowSeconds)
-//         {
-//             BeatDebug.GetComponent<Renderer>().material.color = Color.green;
-//         }
-//         else
-//         {
-//             BeatDebug.GetComponent<Renderer>().material.color = Color.red;
-//         }
-//     }
-// }
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(AudioSource))]
+public class BeatManager : MonoBehaviour
+{
+    public event Action OnPreBeat;
+    public event Action OnBeat;
+    public event Action OnPostBeat;
+
+    [SerializeField] private GameObject beatDebug; // Reference to the cube
+    [SerializeField] private SoundManager soundManager;
+    [SerializeField, Range(0, 180)] private float bpm = 120.0f;
+    [Range(1, 50)] public float MoveWindowTimePercent = 10;
+    [SerializeField] public bool ShouldPerformTicks;
+    private int lastBeatNumber;
+    public float BeatInterval => 60.0f / bpm;
+    public float LastBeatTime { get; set; }
+    public float NextBeatTime { get; set; }
+    public int BeatCounter { get; set; }
+
+    public double MoveWindowSeconds => MoveWindowTimePercent * BeatInterval / 100;
+    private GameController gameController;
+
+    private void Start()
+    {
+        InitMetronome();
+    }
+
+    public void SetBPM(int bpm)
+    {
+        this.bpm = bpm;
+    }
+
+    private void InitMetronome()
+    {
+        LastBeatTime = 0;
+
+        NextBeatTime = LastBeatTime + BeatInterval;
+    }
+
+    public float GetCurrentTime()
+    {
+        return soundManager.MusicSource.timeSamples / (float)soundManager.MusicSource.clip.frequency;
+    }
+
+    public float GetCurrentBeatPosition()
+    {
+        return soundManager.MusicSource.timeSamples / (soundManager.MusicSource.clip.frequency * BeatInterval);
+    }
+
+    private void Awake()
+    {
+        this.gameController = FindObjectOfType<GameController>();
+        OnBeat -=  ScaleDebugElement;
+        OnBeat +=  ScaleDebugElement;
+    }
+
+    private void Update()
+    {
+        beatDebug.transform.localScale = Vector3.Lerp(beatDebug.transform.localScale, Vector3.one, Time.deltaTime*2f);
+        if (gameController.IsGameOver)
+        {
+            return;
+        }
+
+        if (ShouldPerformTicks == false)
+        {
+            InitMetronome();
+            return;
+        }
+
+        float currentTime = GetCurrentTime();
+        float currentBeat = GetCurrentBeatPosition();
+        if (Mathf.FloorToInt(currentBeat) != BeatCounter)
+        {
+            soundManager.PlaySfx(SoundManager.Sfx.DebugBeat3, 1);
+            LastBeatTime = currentTime;
+            NextBeatTime = LastBeatTime + BeatInterval;
+            BeatCounter++;
+            OnBeat?.Invoke();
+            StartCoroutine(ScheduleAction(OnPreBeat, (float)(BeatInterval - MoveWindowSeconds)));
+            StartCoroutine(ScheduleAction(OnPostBeat, (float)MoveWindowSeconds));
+        }
+    }
+
+
+    private IEnumerator ScheduleAction(Action action, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        action?.Invoke();
+    }
+
+
+    public void DebugBeatRange(Color c)
+    {
+        beatDebug.GetComponent<Renderer>().material.color = c;
+    }
+
+    public void ScaleDebugElement()
+    {
+        beatDebug.transform.localScale = Vector3.one * Random.Range(1.3f, 1.5f);
+    }
+}
